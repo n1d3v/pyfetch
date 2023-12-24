1ddb3c008b6c7a77dd9d598a830151db6c0837cb
patricktbp <135556230+n1d3v@users.noreply.github.com>
2023-12-24 19:57:57 +0000

    Add files via upload

--- pyfetch.py
+++ pyfetch.py
@@ -2,6 +2,15 @@ import psutil
 import platform
 import GPUtil as GPU
 import subprocess
+import os
+
+tux_logo = '''    .--.
+   |o_o |
+   |:_/ |
+  //   \\ \\
+ (|     | )
+/'\\_   _/`\\
+\\___)=(___/'''
 
 def get_os_info():
     try:
@@ -25,61 +34,64 @@ def get_cpu_name():
                     return line.rstrip('\n').split(':')[1].strip()
     return 'CPU Name Not Found'
 
+def get_shell():
+    shell = os.environ.get('SHELL')
+    if shell:
+        return shell.split('/')[-1]
+    return "Shell not detected"
+
+def get_desktop_environment():
+    de = os.environ.get('XDG_CURRENT_DESKTOP')
+    if de:
+        return de
+    wm = subprocess.getoutput('echo $XDG_SESSION_DESKTOP')
+    if wm:
+        return wm
+    return "DE/WM not detected"
+
 def fetch_sys_info():
-    # OS info
     os_info = get_os_info()
-
-    # Kernel info
     kernel_version = get_kernel_version()
-
-    # CPU info
     cpu_name = get_cpu_name()
-    cpu_utilization = f"{psutil.cpu_percent()}% utilization"
-
-    # Memory info
+    cpu_utilization = f"{psutil.cpu_percent()}% utilized"
     mem = psutil.virtual_memory()
     mem_used = f"{int(mem.used / (1024 * 1024))}MB / {int(mem.total / (1024 * 1024))}MB"
-
-    # Disk info
-    disk_info = psutil.disk_partitions()
-    disk_details = []
-    for disk in disk_info:
-        if 'rw' in disk.opts:
-            disk_usage = psutil.disk_usage(disk.mountpoint)
-            disk_details.append(f"{disk.device} - {disk.mountpoint}: "
-                                f"{disk_usage.used / (1024 * 1024 * 1024):.2f}GB used / "
-                                f"{disk_usage.total / (1024 * 1024 * 1024):.2f}GB total")
-
-    # GPU info
     gpus = GPU.getGPUs()
-    gpu_info = []
-    for gpu in gpus:
-        gpu_info.append(f"{gpu.name}: {gpu.memoryUsed}MB used / {gpu.memoryTotal}MB total")
+    gpu_info = [f"{gpu.name}" for gpu in gpus]
+    shell_info = get_shell()
+    desktop_env = get_desktop_environment()
 
     system_info = {
         'OS': os_info,
         'Kernel': kernel_version,
+        'Shell': shell_info,
         'CPU': f"{cpu_name} - {cpu_utilization}",
         'Memory': mem_used,
-        'Disks': disk_details,
-        'GPU': ', '.join(gpu_info)
+        'GPU': ', '.join(gpu_info),
+        'DE/WM': desktop_env
     }
     return system_info
 
-def display_sys_info(system_info):
-    for key, value in system_info.items():
-        if key == 'Disks':
-            print("Disks / Partitions:")
-            if value:
-                for disk in value:
-                    print(f"  - {disk}")
+def display_sys_info(system_info, logo):
+    logo_lines = logo.split('\n')
+    max_logo_length = max(len(line) for line in logo_lines)
+
+    total_lines = max(len(logo_lines), len(system_info))
+
+    for line_num in range(total_lines):
+        logo_line = logo_lines[line_num] if line_num < len(logo_lines) else ''
+
+        sys_info_line = list(system_info.items())[line_num] if line_num < len(system_info) else None
+
+        if sys_info_line:
+            key, value = sys_info_line
+            if key == 'GPU':
+                print(f"{logo_line}{' ' * (max_logo_length - len(logo_line))}    {key}: {value}")
             else:
-                print("  - No additional disks / partitions found.")
-        elif key == 'GPU':
-            print(f"{key}: {value}")
+                print(f"{logo_line}{' ' * (max_logo_length - len(logo_line))}    {key}: {value}")
         else:
-            print(f"{key}: {value}")
+            print(f"{logo_line}{' ' * (max_logo_length - len(logo_line))}")
 
 if __name__ == "__main__":
     sys_info = fetch_sys_info()
-    display_sys_info(sys_info)
+    display_sys_info(sys_info, tux_logo)
